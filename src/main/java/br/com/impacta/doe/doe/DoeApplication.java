ab73a7a4b20a4620592ec16c0fb4331e2c0f220b
Arthur <santosalmeida.arthur@gmail.com>
2021-10-30 15:45:22 -0300

    feat: Criando documentação da api

--- src/main/java/br/com/impacta/doe/doe/DoeApplication.java
+++ src/main/java/br/com/impacta/doe/doe/DoeApplication.java
@@ -2,8 +2,10 @@ package br.com.impacta.doe.doe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class DoeApplication {
 
 	public static void main(String[] args) {
