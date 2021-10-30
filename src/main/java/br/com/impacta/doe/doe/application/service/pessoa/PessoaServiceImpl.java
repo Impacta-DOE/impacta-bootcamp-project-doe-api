4d91b1d540c0b2d3f5f044b19afccc4944c06edb
Arthur <santosalmeida.arthur@gmail.com>
2021-10-30 15:18:51 -0300

    criando service para pessoa

---
+++ src/main/java/br/com/impacta/doe/doe/application/service/pessoa/PessoaServiceImpl.java
@@ -0,0 +1,36 @@
+package br.com.impacta.doe.doe.application.service.pessoa;
+
+import br.com.impacta.doe.doe.application.domain.pessoa.Pessoa;
+import br.com.impacta.doe.doe.infra.database.pessoa.PessoaRepository;
+import br.com.impacta.doe.doe.web.pessoa.PessoaDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PessoaServiceImpl implements PessoaService {
+    private PessoaRepository pessoaRepository;
+
+    @Autowired
+    public PessoaServiceImpl(PessoaRepository pessoaRepository) {
+        this.pessoaRepository = pessoaRepository;
+    }
+
+    @Override
+    public Pessoa salva(PessoaDto pessoaDto) {
+        Pessoa pessoa = pessoaDto.converte();
+        return pessoaRepository.salva(pessoa);
+    }
+
+    @Override
+    public Optional<Pessoa> atualiza(Long id, PessoaDto pessoaDto) {
+        Optional<Pessoa> pessoaOptional = pessoaRepository.buscaPorId(id);
+        if (pessoaOptional.isPresent()) {
+            Pessoa pessoa = pessoaDto.converte();
+            pessoa.setId(id);
+            return pessoaRepository.atualiza(pessoa);
+        }
+        return Optional.empty();
+    }
+}
