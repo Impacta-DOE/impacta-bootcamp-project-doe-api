9f234652d89928327731e45f512649e394ceac7a
Arthur <santosalmeida.arthur@gmail.com>
2021-11-10 20:43:52 -0300

    feat: criando gitignore / refactor: alterando tipo de variavel em dto

--- src/main/java/br/com/impacta/doe/doe/web/pessoa/NacionalidadeDto.java
+++ src/main/java/br/com/impacta/doe/doe/web/pessoa/NacionalidadeDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 public class NacionalidadeDto {
-    private String id;
+    private Long id;
     private String descricao;
 
     public NacionalidadeDto(Nacionalidade nacionalidade) {
