4034b02c41a28d4b496cfb3b85bf034fe5dcbb85
Arthur <santosalmeida.arthur@gmail.com>
2021-11-10 20:34:07 -0300

    feat: Implementando classe de implementação para sql server

--- src/main/java/br/com/impacta/doe/doe/application/service/pessoa/PessoaServiceImpl.java
+++ src/main/java/br/com/impacta/doe/doe/application/service/pessoa/PessoaServiceImpl.java
@@ -1,7 +1,7 @@
 package br.com.impacta.doe.doe.application.service.pessoa;
 
 import br.com.impacta.doe.doe.application.domain.pessoa.Pessoa;
-import br.com.impacta.doe.doe.infra.database.pessoa.PessoaRepository;
+import br.com.impacta.doe.doe.infra.database.pessoa.PessoaRepositorySQLServer;
 import br.com.impacta.doe.doe.web.pessoa.PessoaDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,26 +10,26 @@ import java.util.Optional;
 
 @Service
 public class PessoaServiceImpl implements PessoaService {
-    private PessoaRepository pessoaRepository;
+    private PessoaRepositorySQLServer pessoaRepository;
 
     @Autowired
-    public PessoaServiceImpl(PessoaRepository pessoaRepository) {
+    public PessoaServiceImpl(PessoaRepositorySQLServer pessoaRepository) {
         this.pessoaRepository = pessoaRepository;
     }
 
     @Override
     public Pessoa salva(PessoaDto pessoaDto) {
         Pessoa pessoa = pessoaDto.converte();
-        return pessoaRepository.salva(pessoa);
+        return pessoaRepository.save(pessoa);
     }
 
     @Override
     public Optional<Pessoa> atualiza(Long id, PessoaDto pessoaDto) {
-        Optional<Pessoa> pessoaOptional = pessoaRepository.buscaPorId(id);
+        Optional<Pessoa> pessoaOptional = pessoaRepository.findById(id);
         if (pessoaOptional.isPresent()) {
             Pessoa pessoa = pessoaDto.converte();
             pessoa.setId(id);
-            return pessoaRepository.atualiza(pessoa);
+            return Optional.of(pessoaRepository.save(pessoa));
         }
         return Optional.empty();
     }
