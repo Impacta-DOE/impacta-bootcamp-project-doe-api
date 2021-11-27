699c4d7aa8c2e7dbbed037c7c9024a85cc0be151
Arthur <santosalmeida.arthur@gmail.com>
2021-11-27 17:49:51 -0300

    refactor: deletando classes nao serao mais utilizadas..

--- src/main/java/br/com/impacta/doe/doe/infra/database/pessoa/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.impacta.doe.doe.infra.database.pessoa;
-
-import br.com.impacta.doe.doe.application.domain.pessoa.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-}
