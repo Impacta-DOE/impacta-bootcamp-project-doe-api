68d7b20cee089ed415f3089fed15816c675838c1
Arthur <santosalmeida.arthur@gmail.com>
2021-11-27 17:58:03 -0300

    feat: adicionando anotação do lombok.

--- src/main/java/br/com/impacta/doe/doe/application/domain/pessoa/DadosBancarios.java
+++ src/main/java/br/com/impacta/doe/doe/application/domain/pessoa/DadosBancarios.java
@@ -1,18 +1,17 @@
 package br.com.impacta.doe.doe.application.domain.pessoa;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 public class DadosBancarios {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToOne
-    private TipoConta tipoConta;
-    @OneToOne
-    private Pessoa pessoa;
     private String banco;
     private String agencia;
     private String conta;
-    private String digitoConta;
+    private String codigoBanco;
 }
--- src/main/java/br/com/impacta/doe/doe/application/domain/pessoa/Endereco.java
+++ src/main/java/br/com/impacta/doe/doe/application/domain/pessoa/Endereco.java
@@ -1,14 +1,15 @@
 package br.com.impacta.doe.doe.application.domain.pessoa;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 public class Endereco {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToOne
-    private Pessoa pessoa;
     private String cep;
     private String logradouro;
     private String numero;
