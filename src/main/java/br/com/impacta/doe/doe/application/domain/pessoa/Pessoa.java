3dcc02ea9a17d36c0795c8c8c038eb69b0ecab93
Arthur <santosalmeida.arthur@gmail.com>
2021-11-27 17:54:08 -0300

    refactor: tornando a classe pessoa em classe pai.

--- src/main/java/br/com/impacta/doe/doe/application/domain/pessoa/Pessoa.java
+++ src/main/java/br/com/impacta/doe/doe/application/domain/pessoa/Pessoa.java
@@ -3,7 +3,6 @@ package br.com.impacta.doe.doe.application.domain.pessoa;
 import lombok.Data;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 
 @Data
 @Entity
@@ -11,16 +10,13 @@ public class Pessoa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Enumerated(EnumType.STRING)
-    private TipoPessoa tipoPessoa;
-    private String documento;
-    private String nome;
-    private String email;
-    private String telefone;
-    private LocalDate dataInclusao;
-    private LocalDate dataNascimento;
-    private Sexo sexo;
     @OneToOne
-    private Nacionalidade nacionalidade;
-    private LocalDate dataAtualizacao;
+    private DadosContato dadosContato;
+    @OneToOne
+    private DadosBancarios dadosBancarios;
+    @OneToOne
+    private Endereco endereco;
+
+    public Pessoa() {
+    }
 }
