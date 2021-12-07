4511f128a9ac8fd7c3f754994a199d207f8f8206
Arthur <santosalmeida.arthur@gmail.com>
2021-12-07 01:43:47 -0300

    refactor: acrescentando atributos da antiga classe pessoa nestas classes e colocando os relacionamentos com cascade type persist.

--- src/main/java/br/com/impacta/doe/doe/application/domain/pessoa/PessoaFisica.java
+++ src/main/java/br/com/impacta/doe/doe/application/domain/pessoa/PessoaFisica.java
@@ -2,17 +2,27 @@ package br.com.impacta.doe.doe.application.domain.pessoa;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @Entity
-public class PessoaFisica extends Pessoa {
+public class PessoaFisica {
+    //TODO: extrair estes atributos para uma classe pessoa e utilizar herança
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne(cascade=CascadeType.PERSIST)
+    private DadosContato dadosContato;
+    @OneToOne(cascade=CascadeType.PERSIST)
+    private DadosBancarios dadosBancarios;
+    @OneToOne(cascade=CascadeType.PERSIST)
+    private Endereco endereco;
+
     private String nomeCompleto;
     private LocalDate dataNascimento;
     private String sexo;
-    @OneToOne
+    @OneToOne(cascade=CascadeType.PERSIST)
     private Nacionalidade nacionalidade;
     private String registro;
 
--- src/main/java/br/com/impacta/doe/doe/application/domain/pessoa/PessoaJuridica.java
+++ src/main/java/br/com/impacta/doe/doe/application/domain/pessoa/PessoaJuridica.java
@@ -2,11 +2,22 @@ package br.com.impacta.doe.doe.application.domain.pessoa;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Data
 @Entity
-public class PessoaJuridica extends Pessoa {
+public class PessoaJuridica {
+    //TODO: extrair estes atributos para uma classe pessoa e utilizar herança
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne(cascade=CascadeType.PERSIST)
+    private DadosContato dadosContato;
+    @OneToOne(cascade=CascadeType.PERSIST)
+    private DadosBancarios dadosBancarios;
+    @OneToOne(cascade=CascadeType.PERSIST)
+    private Endereco endereco;
+
     private String cnpj;
     private String razaoSocial;
 
