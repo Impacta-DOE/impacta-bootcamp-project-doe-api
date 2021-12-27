b5a87e52a508bbf220586d9aa1440c0b1581ea56
Arthur <santosalmeida.arthur@gmail.com>
2021-12-27 19:36:57 -0300

    refactor: Retirando import inutilizado

--- src/main/java/br/com/impacta/doe/doe/web/pessoa/juridica/PessoaJuridicaController.java
+++ src/main/java/br/com/impacta/doe/doe/web/pessoa/juridica/PessoaJuridicaController.java
@@ -1,7 +1,6 @@
 package br.com.impacta.doe.doe.web.pessoa.juridica;
 
 import br.com.impacta.doe.doe.application.service.pessoa.juridica.PessoaJuridicaService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
